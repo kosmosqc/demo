8f95397dd6797dbaae3a5984f10ece6420b2d72a
Fairl <Fairl@MAIN>
2021-10-09 15:41:45 -0400

    last

--- src/Composants/Accueil.js
+++ src/Composants/Accueil.js
@@ -9,13 +9,21 @@ class Accueil extends Component {
     render() {
 
         return (
-            <div style={{display: "flex", justifyContent: "left" , color : "white", margin : "150px", textAlign: "left"}}>
-                <h1 style={{fontSize: "50px"}}>Bonjour ,<br />
-                    Je suis Alain,<br />
-                    developpeur web
-                    <p style={{fontSize: "20px"}}>Je asdasdas</p>
+            <div style={{ display: "flex", justifyContent: "left", color: "white", margin: "150px", textAlign: "left" }}>
+                <h1 style={{ fontSize: "50px" }}>Bonjour ,<br />
+                    je suis Alain,<br />
+                    developpeur web (Full-stack)
+                    <br />
+                    <div style={{ display: "flex", justifyContent: "center", color: "white", margin: "150px", textAlign: "left" }}>
+                        <br /><p style={{ fontSize: "20px" }}>Je suis un programmeur junior motivé, débrouillard et je possède une excellente capacité d'analyse et de résolution de problème<br />
+                            <br />Skill&nbsp; <br />
+                            Languages &nbsp;&nbsp;: &nbsp; Javascript,&nbsp; Java,&nbsp; C#,&nbsp; C++,&nbsp; PHP,&nbsp; HTML et SQL.<br />
+                            Framework : &nbsp; React,&nbsp; ASP.net<br />
+                            Outil&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : &nbsp; Windows 10,&nbsp; Powershell,&nbsp; Linux,&nbsp; VS code,&nbsp;, Git hub<br />
+                        </p>
+                    </div>
                 </h1>
-                
+
             </div>
         );
     }
