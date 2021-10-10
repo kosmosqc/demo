0968dff1827855b1d468e674e952bf672f85f1e2
Fairl <Fairl@MAIN>
2021-10-10 00:53:34 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Grid, Menu } from 'semantic-ui-react';
 import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 import { useWindowsSize } from './Composants/useWindowsSize';
