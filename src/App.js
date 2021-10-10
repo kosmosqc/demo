80131de86e59dca2466308c7a7d2f70ff09ae6f1
Fairl <Fairl@MAIN>
2021-10-10 17:54:49 -0400

    new

--- src/App.js
+++ src/App.js
@@ -13,38 +13,56 @@ function App() {
   let size = "25px"
   let h = "225px"
   const [load, setLoad] = useState(0)
- 
+
   if (largeur < 600) {
     size = "10px"
     h = "90px"
   }
-
+console.log(load)
   document.title = "Alain Gionet"
 
   if (load === 0) {
     return (
-      <div class="main">
-        <div >
-          <button class="click" onClick={() => setLoad(load + 1)}>
-            Click me!
-          </button>
-        </div>
-
+      <div className="main">
+      <div  >
+        <React.Fragment >
+          <BrowserRouter>
+            <h1 style={{ display: "flex", height: h, width: "150px", margin: "10px" }}>
+              <Menu style={{ backgroundColor: "#5f43b2"}}>
+                <Grid >
+                  <Grid.Column  >
+                    <Menu.Item as={NavLink} onClick={() =>load <1 ? setLoad(load - 0): setLoad(load - 1)} style={{ color: "#96c2ff", fontSize: size }} activeStyle={{ fontWeight: "bold", color: "white" }} to="/" exact={true}>Accueil   </Menu.Item>
+                    <Menu.Item as={NavLink} onClick={() =>load >0 ? setLoad(load - 0): setLoad(load + 1)} style={{ color: "#96c2ff", fontSize: size }} activeStyle={{ fontWeight: "bold", color: "white" }} to="/about" exact={true}>About   </Menu.Item>
+                    <Menu.Item as={NavLink} style={{ color: "#96c2ff", fontSize: size }} activeStyle={{ fontWeight: "bold", color: "white" }} to="/contact" exact={true}>Contact   </Menu.Item>
+                  </Grid.Column>
+                </Grid>
+              </Menu>
+            </h1>
+            <div style={{ marginTop: "-130px" }}>
+              <Switch>
+                <Route path="/"  exact={true} />
+                <Route path="/About" component={Accueil} exact={true} />
+                <Route path="/contact" component={Contact} exact={true} />
+              </Switch>
+            </div>
+          </BrowserRouter>
+        </React.Fragment>
+      </div>
       </div>
     )
   }
   else {
     return (
       <div className="back">
-        <div className="twinkling" >
+        
           <React.Fragment >
             <BrowserRouter>
               <h1 style={{ display: "flex", margin: "10px", height: h, width: "150px" }}>
-                <Menu  style={{ backgroundColor: "#5f43b2" }}>
+                <Menu style={{ backgroundColor: "#5f43b2" }}>
                   <Grid >
                     <Grid.Column  >
-                      <Menu.Item as={NavLink } onClick={() => setLoad(load - 1)} style={{ color: "#96c2ff", fontSize: size }} activeStyle={{ fontWeight: "bold" }} to="/about" exact={true}>Accueil   </Menu.Item>
-                      <Menu.Item as={NavLink}  style={{ color: "#96c2ff", fontSize: size }} activeStyle={{ fontWeight: "bold", color: "white" }} to="/About" exact={true}>About   </Menu.Item>
+                      <Menu.Item as={NavLink} onClick={() => load <1 ? setLoad(load - 0): setLoad(load - 1)} style={{ color: "#96c2ff", fontSize: size }} activeStyle={{ fontWeight: "bold" , color: "white" }} to="/" exact={true}>Accueil   </Menu.Item>
+                      <Menu.Item as={NavLink} onClick={() => load >0 ? setLoad(load - 0): setLoad(load + 1)} style={{ color: "#96c2ff", fontSize: size }} activeStyle={{ fontWeight: "bold", color: "white" }} to="/about" exact={true}>About   </Menu.Item>
                       <Menu.Item as={NavLink} style={{ color: "#96c2ff", fontSize: size }} activeStyle={{ fontWeight: "bold", color: "white" }} to="/contact" exact={true}>Contact   </Menu.Item>
                     </Grid.Column>
                   </Grid>
@@ -52,7 +70,7 @@ function App() {
               </h1>
               <div style={{ marginTop: "-130px" }}>
                 <Switch>
-                 
+                <Route path="/" component={Accueil} exact={true} />
                   <Route path="/About" component={Accueil} exact={true} />
                   <Route path="/contact" component={Contact} exact={true} />
                 </Switch>
@@ -60,7 +78,7 @@ function App() {
             </BrowserRouter>
           </React.Fragment>
         </div>
-      </div>
+      
     );
   }
 }
--- src/Composants/Accueil.js
+++ src/Composants/Accueil.js
@@ -11,23 +11,56 @@ class Accueil extends Component {
                     Alain Gionet,<br />
                     developpeur web (Full-stack)
                     <br />
-                    <div class="cont">
+                    <div class="cont" >
                         <br /><p >Programmeur junior motivé, débrouillard et possède une excellente capacité d'analyse et de résolution de problème.<br />
-                            <br />Skill :&nbsp; <br />
-                            <ul>
+                            <div>
+                                <br />Skill :&nbsp; <br />
                                 <li>
                                     <span>Javascript</span>
-                                    
-                                        <span>Javascript</span>
-                                    
                                 </li>
                                 <li>
                                     <span>Java</span>
                                 </li>
-                            </ul>
-                            Languages &nbsp;&nbsp;: &nbsp; Javascript,&nbsp; Java,&nbsp; C#,&nbsp; C++,&nbsp; PHP,&nbsp; HTML et SQL.<br />
-                            Framework : &nbsp; React,&nbsp; ASP.net<br />
-                            Outil&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : &nbsp; Windows 10,&nbsp; Powershell,&nbsp; Linux,&nbsp; VS code,&nbsp; Git hub<br />
+                                <li>
+                                    <span>C#</span>
+                                </li>
+                                <li>
+                                    <span>C++</span>
+                                </li>
+                                <li>
+                                    <span>PHP</span>
+                                </li>
+                                <li>
+                                    <span>HTML</span>
+                                </li>
+                                <li>
+                                    <span>SQL</span>
+                                </li>
+                                <li>
+                                    <span>React</span>
+                                </li>
+                                <li>
+                                    <span>Asp.net</span>
+                                </li>
+                            </div>
+                            <div class="list">
+                            <br /> Outil :
+                                <li>
+                                    <span>Windows 10</span>
+                                </li>
+                                <li>
+                                    <span>Powershell</span>
+                                </li>
+                                <li>
+                                    <span>Linux</span>
+                                </li>
+                                <li>
+                                    <span>VS code</span>
+                                </li>
+                                <li>
+                                    <span>Git hub</span>
+                                </li>
+                            </div>
                         </p>
                     </div>
                 </h1>
