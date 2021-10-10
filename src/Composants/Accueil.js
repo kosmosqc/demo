d78da883e95ca316bf4f2a945047294631453db1
Fairl <Fairl@MAIN>
2021-10-10 16:44:08 -0400

    new

--- src/Composants/Accueil.js
+++ src/Composants/Accueil.js
@@ -13,7 +13,18 @@ class Accueil extends Component {
                     <br />
                     <div class="cont">
                         <br /><p >Programmeur junior motivé, débrouillard et possède une excellente capacité d'analyse et de résolution de problème.<br />
-                            <br />Skill&nbsp; <br />
+                            <br />Skill :&nbsp; <br />
+                            <ul>
+                                <li>
+                                    <span>Javascript</span>
+                                    
+                                        <span>Javascript</span>
+                                    
+                                </li>
+                                <li>
+                                    <span>Java</span>
+                                </li>
+                            </ul>
                             Languages &nbsp;&nbsp;: &nbsp; Javascript,&nbsp; Java,&nbsp; C#,&nbsp; C++,&nbsp; PHP,&nbsp; HTML et SQL.<br />
                             Framework : &nbsp; React,&nbsp; ASP.net<br />
                             Outil&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : &nbsp; Windows 10,&nbsp; Powershell,&nbsp; Linux,&nbsp; VS code,&nbsp; Git hub<br />
