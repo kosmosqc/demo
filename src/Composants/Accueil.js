d5d9d383e7f78d4a63ff6d881551623e6a568d7a
Fairl <Fairl@MAIN>
2021-10-13 22:53:35 -0400

    n

--- src/Composants/Accueil.js
+++ src/Composants/Accueil.js
@@ -1,10 +1,10 @@
 import '../App.css';
 
 const Accueil = () => {
-    return(
-        <div>
-          <h1 style={{ color: "white" }}>Alain Gionet</h1>  
-        </div>
-    )
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>Alain Gionet</h1>
+    </div>
+  )
 }
 export default Accueil
