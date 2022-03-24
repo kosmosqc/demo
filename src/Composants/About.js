6d1bb6b76a2d0201fd0616d28b6937393085af03
Fairl <Fairl@MAIN>
2022-03-23 21:46:53 -0400

    gg

--- src/Composants/About.js
+++ src/Composants/About.js
@@ -1,56 +1,102 @@
-import '../App.css';
+import '../Skill.css';
 import React from 'react';
-
 function About() {
 
 
     return (
-        <div className="accueil" style={{ color: "white", marginLeft: "130px", textAlign: "left" }}>
-            <div className="title">
-                <p > Alain Gionet,<br />
-                    Développeur web (Full-stack)</p>
-                <div >
-                    <h1 > <p className="st">Programmeur junior motivé, débrouillard et possède une excellente capacité d'analyse et de résolution de problème.</p>
-                    
-                        <div className="cont">
-                            <br /><p style={{ color: "#96c2ff" }}>Skill :</p>
-                            <ul>
-                                <li>Javascript</li>
-                                <li>React.js</li>
-                                <li>C#</li>
-                                <li>C++</li>
-                                <li>PHP</li>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>SQL</li>
-                                <li>Java</li>
-                                <li>Asp.net</li>
-                            </ul>
-                        </div>
-                        <div className="cont">
-                            <br/> <p style={{ color: "#96c2ff" }}>Outil :</p>
-                            <ul>
-                                <li>Windows 10</li>
-                                <li>Powershell</li>
-                                <li>Linux</li>
-                                <li>Visual code/studio</li>
-                                <li>Git hub</li>
-                                <li>PhotoShop</li>
-                            </ul>
-                        </div>
-                        <div className="cont">
-                            <br/> <p style={{ color: "#96c2ff" }}>Passion :</p>
-                            <ul>
-                                <li>Informatique</li>
-                                <li>Astronomie</li>
-                                <li>Science</li>
-                            </ul>
-                        </div>
-                    </h1>
-                </div>
+
+
+        <div class="container">
+            <h3 style={{color : "white" , textAlign : 'center' , fontSize : "26px"}}> Alain Gionet<br />
+                Programmeur (Full-stack) motivé, débrouillard et possède une excellente capacité d'analyse et de résolution de problème.</h3>
+            <div class="col">
+                <h2 class="titulo">Skill</h2>
+                <ul class="skill">
+                    <li><span class="blueline javascript"></span><em>Javascript</em></li>
+                    <li><span class="blueline react"></span><em>React.js</em></li>
+                    <li><span class="blueline csharp"></span><em>C#</em></li>
+                    <li><span class="blueline cplus"></span><em>C++</em></li>
+                    <li><span class="blueline php "></span><em>PHP</em></li>
+                    <li><span class="blueline html"></span><em>HTML</em></li>
+                    <li><span class="blueline css"></span><em>CSS</em></li>
+                    <li><span class="blueline sql"></span><em>SQL</em></li>
+                    <li><span class="blueline java"></span><em>Java</em></li>
+                    <li><span class="blueline Asp"></span><em>Asp.net</em></li>
+
+                </ul>
+            </div>
+            <div class="col">
+                <h2 class="plataformas titulo">Outil</h2>
+                <ul class="skill">
+                    <li><span class="greenline win10"></span><em>Windows 10</em></li>
+                    <li><span class="greenline powershell"></span><em>Powershell</em></li>
+                    <li><span class="greenline linux"></span><em>Linux</em></li>
+                    <li><span class="greenline visual"></span><em>Visual code/studio</em></li>
+                    <li><span class="greenline git"></span><em>Git Hub</em></li>
+                    <li><span class="greenline ps"></span><em>PhotoShop</em></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h2 class="programas titulo">Passion</h2>
+                <ul class="skill">
+                    <li><span class="redline informatique"></span><em>Informatique</em></li>
+                    <li><span class="redline astronomie"></span><em>Astronomie</em></li>
+                    <li><span class="redline science"></span><em>Science</em></li>
                
+                </ul>
             </div>
         </div>
+
+
+
+
+
+
+        // <div className="accueil" style={{ color: "white", marginLeft: "130px", textAlign: "left" }}>
+        //     <div className="title">
+        //         <p > Alain Gionet,<br />
+        //             Programmeur (Full-stack)</p>
+        //         <div >
+        //             <h1 > <p className="st">Programmeur junior motivé, débrouillard et possède une excellente capacité d'analyse et de résolution de problème.</p>
+
+        //                 <div className="cont">
+        //                     <br /><p style={{ color: "#96c2ff" }}>Skill :</p>
+        //                     <ul>
+        //                         <li>Javascript <img src={palette} alt=""></img></li>
+        //                         <li>React.js</li>
+        //                         <li>C#</li>
+        //                         <li>C++</li>
+        //                         <li>PHP</li>
+        //                         <li>HTML</li>
+        //                         <li>CSS</li>
+        //                         <li>SQL</li>
+        //                         <li>Java</li>
+        //                         <li>Asp.net</li>
+        //                     </ul>
+        //                 </div>
+        //                 <div className="cont">
+        //                     <br /> <p style={{ color: "#96c2ff" }}>Outil :</p>
+        //                     <ul>
+        //                         <li>Windows 10</li>
+        //                         <li>Powershell</li>
+        //                         <li>Linux</li>
+        //                         <li>Visual code/studio</li>
+        //                         <li>Git hub</li>
+        //                         <li>PhotoShop</li>
+        //                     </ul>
+        //                 </div>
+        //                 <div className="cont">
+        //                     <br /> <p style={{ color: "#96c2ff" }}>Passion :</p>
+        //                     <ul>
+        //                         <li>Informatique</li>
+        //                         <li>Astronomie</li>
+        //                         <li>Science</li>
+        //                     </ul>
+        //                 </div>
+        //             </h1>
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
 export default About;
--- src/Composants/Contact.js
+++ src/Composants/Contact.js
@@ -1,50 +1,64 @@
 import { useState } from "react";
 import React from 'react';
 import '../App.css';
-
+import palette from "../color-palette.png"
 
 //show
 function Couleur(props) {
+  const [isShown, setIsShown ] = useState(false);
+  const [text, setText ] = useState(" ");
+ 
+ 
+  var  text1 = " Petit projet personnel : une extension en version beta pour firefox qui permet d'arrondir les prix sur le site d'Amazon. Example : 3.99$ devient 4$ "
+  
+
+  
   return (
+
     <div className="link" style={{ display: 'flex', justifyContent: 'center', color: props.textcolor.couleur, textAlign: "center", marginTop: "150px" }} >
+
       <div className="contact-menu">
+
         <ul >
-          <li><a href="https://www.linkedin.com/in/alain-gionet-8899a1220/"><i style={{ color: props.textcolor.couleur }} className="fab fa-linkedin-in"></i></a></li>
-          <li><a href="mailto:kosmosqc@gmail.com"><i style={{ color: props.textcolor.couleur }} className="fas fa-at"></i></a></li>
-          <li><a href="https://github.com/kosmosqc?tab=repositories"><i style={{ color: props.textcolor.couleur }} className="fab fa-git"></i></a></li>
-          <li><a href="https://replit.com/@AlainGionet/Tic-tac-toe-1?v=1"><i style={{ color: props.textcolor.couleur }} className="fas fa-gamepad"></i></a></li>
+          <li><a onMouseOver={() => setText(text1)} onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} href="https://addons.mozilla.org/en-US/firefox/addon/rounded-price-for-amazon/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search"><i style={{ color: props.textcolor.couleur }} className="fab fa-bolt"></i></a></li>
+          <li><a onMouseOver={() => setText("Linkedin")} onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} href="https://www.linkedin.com/in/alain-gionet-8899a1220/"><i style={{ color: props.textcolor.couleur }} className="fab fa-linkedin-in"></i></a></li>
+          <li><a onMouseOver={() => setText("kosmosqc@gmail.com")} onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} href="mailto:kosmosqc@gmail.com"><i style={{ color: props.textcolor.couleur }} className="fas fa-at"></i></a></li>
+          <li><a onMouseOver={() => setText("Git hub")} onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} href="https://github.com/kosmosqc?tab=repositories"><i style={{ color: props.textcolor.couleur }} className="fab fa-git"></i></a></li>
+          <li><a onMouseOver={() => setText("Jeux de tic tac toe en Javascript")} onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} href="https://replit.com/@AlainGionet/Tic-tac-toe-1?v=1"><i style={{ color: props.textcolor.couleur }} className="fas fa-gamepad"></i></a></li>
         </ul>
+        {isShown && (
+          <div>
+            <p style={{ color : "white", marginTop : "20px"}}>
+              {text}
+
+            </p>
+          </div>
+        )}
       </div>
+
     </div>
   )
 }
 
 function Contact() {
 
-  const text = [{ couleur: "black" }, { couleur: "#96c2ff" }, { couleur: "aqua" }, { couleur: "red" }, { couleur: "orange" }, { couleur: "purple" }]
-  let show = false
+  const text = [{ couleur: "#96c2ff" }, { couleur: "grey" }, { couleur: "aqua" }, { couleur: "red" }, { couleur: "orange" }, { couleur: "purple" }]
+
   const [move, setMove] = useState(20)
   const [i, setColor] = useState(0)
-  
-  if (move >= 120) {
-    show = true
-  }
-  else {
-    show = false
-  }
+
+  const disableButton = move >= 120 ? (<button onClick={() => move < 120 ? setMove(move) : setMove(move - 200)}>  ↑ </button>) : (<button disabled>↑</button>)
+
   return (
     <div className="contact">
 
       <h1>
-        {show &&
-          <button onClick={() => move < 120 ? setMove(move) : setMove(move - 200)}>
-            Haut
-          </button>
-        }
+        {disableButton}
         <button onClick={() => move > 800 ? setMove(move) : setMove(move + 200)}>
-          Bas
+          ↓
         </button>
-        <button onClick={() => i < 5 ? setColor(i + 1) : setColor(i - 5)}>Changer la couleur</button>
+        <button onClick={() => i < 5 ? setColor(i + 1) : setColor(i - 5)}><img src={palette} alt="" style={{ height: "35px", width: "35px" }} /></button>
+
       </h1>
 
       <div style={{ marginTop: move + "px" }}>
